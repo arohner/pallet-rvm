d42b65954fd6d3ef2ded8ac07853dbd03276a40b
Allen Rohner <arohner@gmail.com>
2012-02-28 12:46:46 -0800

    Make clojure a dev dependency, to make CircleCI happy.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject pallet-rvm "0.3.1"
   :description "A pallet crate to install and manage RVM (Ruby Version Manager)"
-  :dependencies [[org.cloudhoist/pallet "0.6.5"]])
+  :dependencies [[org.cloudhoist/pallet "0.6.5"]]
+  :dev-dependencies [[org.clojure/clojure "1.3.0"]])
